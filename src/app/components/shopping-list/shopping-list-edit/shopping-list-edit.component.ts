4e65e9880d91b03bae3e91d62952fa94359524b1
Federico Levin <federico.levin@despegar.com>
2020-03-29 17:24:37 -0300

    Lesson 79: Allowing the user to add ingredients to the Shopping List (with delete working bad)

--- src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output, Input } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.scss']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @Input() ingredient: Ingredient;
+  @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  @Output() ingredientDeleted = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(): void {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+
+    if (ingName !== '' && ingAmount !== '' && ingAmount > 0) {
+      this.ingredientAdded.emit(newIngredient);
+      this.clearItem();
+    }
+  }
+
+  onDeleteItem(): void {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const deletedIngredient = new Ingredient(ingName, ingAmount);
+
+    if (ingName !== '') {
+      this.ingredientDeleted.emit(deletedIngredient);
+      this.clearItem();
+    }
+  }
+
+  clearItem(): void {
+    this.nameInputRef.nativeElement.value = '';
+    this.amountInputRef.nativeElement.value = ''; 
+  }
 }
--- src/app/components/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list/shopping-list.component.ts
@@ -8,6 +8,7 @@ import { Ingredient } from '../../../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredient: Ingredient = new Ingredient('', 0);  
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -16,6 +17,19 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+  }
+
+  onIngredientDeleted(ingredient: Ingredient): void {
+    const indexToDelete = this.ingredients.indexOf(ingredient);
+    this.ingredients.splice(indexToDelete, 1);
+  }
+
+  onIngredientClicked(ingredient: Ingredient): void {
+    this.ingredient = ingredient;
   }
 
 }
