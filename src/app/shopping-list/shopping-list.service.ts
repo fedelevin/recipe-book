dbb491fba7c6322d14bcbb5426022d22715122b0
Federico Levin <federico.levin@despegar.com>
2020-04-12 19:54:14 -0300

    Lesson 109: Passing Ingredients from Recipes to the Shopping List

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,10 +4,7 @@ import { EventEmitter } from "@angular/core";
 export class ShoppingListService {
     ingredientsChanged = new EventEmitter<Ingredient[]>();
 
-    private ingredients: Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10),
-    ];
+    private ingredients: Ingredient[] = [];
 
     getIngredients(): Ingredient[] {
         return this.ingredients.slice();
@@ -16,5 +13,10 @@ export class ShoppingListService {
     addIngredient(ingredient: Ingredient): void {
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]): void {
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients);
     }
 }
