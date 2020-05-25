1ab06e639cb00be40dcf842f9a1c5b95a94e0319
Federico Levin <federico.levin@despegar.com>
2020-05-24 21:12:43 -0300

    Lesson 233: Sending POST request + Getting data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface PostData {
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  private baseURL = 'https://angular-complete-guide-ca57c.firebaseio.com/';
+  private postsURL = this.baseURL + 'posts.json';
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: PostData) {
     // Send Http request
-    this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    this.httpClient.post(
+      this.postsURL,
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    // Send Http request
+    this.httpClient.get(
+      this.postsURL
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 }
