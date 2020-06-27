e1f97f7cc8b1956696176746d1224cda27d47af6
Federico Levin <federico.levin@despegar.com>
2020-06-27 16:50:48 -0300

    @angular/cli migration - lazy-loading-syntax Lazy loading syntax migration. Update lazy loading syntax to use dynamic imports.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
-  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'}
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
+  { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)}
 ];
 
 @NgModule({
